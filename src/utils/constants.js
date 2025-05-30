53a9c6fb00c409246613ac18fae9c47d01c94896
Varun <varunmulukalla@gmail.com>
2025-05-30 12:14:55 +0530

    Update frontend configuration for Vercel deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,8 @@ export const CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "FeastFinder";
 
-// Updated to use our backend proxy
-export const RESTAURANT_API_URL = "/api/restaurants";
-export const MENU_API_URL = "/api/menu/";
+export const API_BASE_URL = "https://feastfinderapi.onrender.com";
+export const RESTAURANT_API_URL = `${API_BASE_URL}/api/restaurants`;
+export const MENU_API_URL = `${API_BASE_URL}/api/menu/`;
 // export const MENU_API_URL =
 //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.5235373&lng=78.2789365&restaurantId=";
